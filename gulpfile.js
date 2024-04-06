ca55ef21ca9b39dcb22df0ce52707c40140d5edb
André Moreira <tpsandre19@gmail.com>
2024-04-06 15:47:51 -0300

    adicionado arquivos do projeto na branch com o nome solicitado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
-const ImageMin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function ComprimiImagem() {
   return gulp
@@ -8,4 +8,14 @@ function ComprimiImagem() {
     .pipe(imagemin())
     .pipe(gulp.dest("./ImagesComprimidas"));
 }
+
+gulp.task("compress-js", function () {
+  return gulp
+    .src("./main.js") // Caminho do arquivo JavaScript original
+    .pipe(uglify()) // Comprimir o arquivo JavaScript
+    .pipe(gulp.dest("./JsComprimido")); // Diretório de saída dos arquivos comprimidos
+});
+
+gulp.task("default", gulp.series("compress-js"));
+
 exports.images = ComprimiImagem;
